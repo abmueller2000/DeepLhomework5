afa27cf01f746f36c725d1284d3e59a7890feafe
abmueller2000 <abmueller2000@gmail.com>
2023-11-13 01:43:51 +0000

    new code

--- homework/controller.py
+++ homework/controller.py
@@ -14,7 +14,7 @@ def control(aim_point, current_vel):
     action = pystk.Action()
 
     # Constants
-    base_target_velocity = 41
+    base_target_velocity = 44
     steer_scale = 3.2
     max_steering_angle = 1
     drift_threshold = 0.70
